5044ac1941f711f53654e9bf857f8256b992cbd5
calebcordry <caleb.cordry@gmail.com>
2017-07-06 17:52:29 -0500

    googs

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -12,46 +12,32 @@
 var addTwoNumbers = function(l1, l2) {
   if (!l1 || !l2) { return; }
     
+  const dummy = new ListNode(0);
+  let current = dummy;
   let carry = 0;
   let node1 = l1;
   let node2 = l2;
-  let sum = node1.val + node2.val + carry;
-  carry = 0;
-    
-  if (sum >= 10) {
+
+  while (node1 || node2) {
+    const val1 = node1 ? node1.val : 0;
+    const val2 = node2 ? node2.val : 0;
+
+    let sum = val1 + val2 + carry;
+      
     carry = Math.floor(sum / 10);
     sum %= 10;
-  }
-        
-  const root = new ListNode(sum);
-    
-  let current = root;
-  node1 = node1.next;
-  node2 = node2.next;
-    
-  while (node1 || node2) {
-    if (!node1) { node1 = new ListNode(0); }
-    if (!node2) { node2 = new ListNode(0); }
-
-    sum = node1.val + node2.val + carry;
-    carry = 0;
-        
-    if (sum >= 10) {
-      carry = Math.floor(sum / 10);
-      sum %= 10;
-    }
         
     const next = new ListNode(sum);
     current.next = next;
         
     current = next;
-    node1 = node1.next;
-    node2 = node2.next;
+    node1 = node1 && node1.next;
+    node2 = node2 && node2.next;
   }
     
   if (carry) {
     current.next = new ListNode(carry);
   }
     
-  return root;
+  return dummy.next;
 };
